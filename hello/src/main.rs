4575440080a146f39064c03cc950c9da5e5f05f9
Yuki Nishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2024-11-15 13:14:02 +0900

    step 1

---
+++ hello/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    
+    for stream in listener.incoming() {
+        handle_connection(stream.unwrap());
+        // println!("Connection established");
+    }
+}
+
+// read data from TCP stream
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 400 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();    
+}
